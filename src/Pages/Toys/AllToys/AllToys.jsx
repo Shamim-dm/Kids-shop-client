86a1e576556733a5f6a8cce53d0232c35bcbcd51
Shamim-DM <mdshamimmiah000000@gmail.com>
2023-05-18 02:51:26 +0600

    modal imformation add

--- src/Pages/Toys/AllToys/AllToys.jsx
+++ src/Pages/Toys/AllToys/AllToys.jsx
@@ -34,17 +34,17 @@ const AllToys = () => {
               <p className="text-red-800 font-bold">
                 Available Quantity: <span></span>
               </p>
-              <button className="btn bg-primary">View Deatils</button>
+              <label htmlFor="my-modal-5" className="btn btn-primary">
+                Views Details
+              </label>
+              {/* <button className="btn bg-primary">View Deatils</button> */}
             </div>
           </div>
           <hr className="mt-3" />
         </div>
 
-
-
-
         <div className="mt-6">
-        <div className="md:flex gap-4">
+          <div className="md:flex gap-4">
             <div>
               <img
                 className="h-36"
@@ -69,18 +69,16 @@ const AllToys = () => {
               <p className="text-red-800 font-bold">
                 Available Quantity: <span></span>
               </p>
-              <button className="btn bg-primary">View Deatils</button>
+              <label htmlFor="my-modal-5" className="btn btn-primary">
+                Views Details
+              </label>
             </div>
           </div>
           <hr className="mt-3" />
         </div>
 
-
-
-
-
         <div className="mt-6">
-        <div className="md:flex gap-4">
+          <div className="md:flex gap-4">
             <div>
               <img
                 className="h-36"
@@ -105,19 +103,16 @@ const AllToys = () => {
               <p className="text-red-800 font-bold">
                 Available Quantity: <span></span>
               </p>
-              <button className="btn bg-primary">View Deatils</button>
+              <label htmlFor="my-modal-5" className="btn btn-primary">
+                Views Details
+              </label>
             </div>
           </div>
           <hr className="mt-3" />
         </div>
 
-
-
-
-
-
         <div className="mt-6">
-        <div className="md:flex gap-4">
+          <div className="md:flex gap-4">
             <div>
               <img
                 className="h-36"
@@ -142,18 +137,49 @@ const AllToys = () => {
               <p className="text-red-800 font-bold">
                 Available Quantity: <span></span>
               </p>
-              <button className="btn bg-primary">View Deatils</button>
+              <label htmlFor="my-modal-5" className="btn btn-primary">
+                Views Details
+              </label>
             </div>
           </div>
           <hr className="mt-3" />
         </div>
+      </div>
 
+      {/* Put this part before </body> tag */}
 
-
-
-
-
-
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box w-11/12 max-w-5xl">
+          <img
+            className="mx-auto w-48 md:w-96"
+            src="https://m.media-amazon.com/images/I/51PSTLPvIxL._AC_SX425_.jpg"
+            alt=""
+          />
+          <h3 className="font-bold text-sky-600 text-lg">
+            Toy Name: <span></span>
+          </h3>
+          <div className="py-4">
+            <h3 className="text-red-800 font-bold">
+              Seller: <span></span>
+            </h3>
+            <p className="text-red-800 font-bold">
+              Price: <span></span>
+            </p>
+            <p className="text-red-800 font-bold">
+              Sub Category: <span></span>
+            </p>
+            <p className="text-red-800 font-bold">
+              Available Quantity: <span></span>
+            </p>
+          </div>
+          <div className="modal-action">
+            <label htmlFor="my-modal-5" className="btn">
+              Close!
+            </label>
+          </div>
+        </div>
       </div>
 
       <Footer></Footer>
