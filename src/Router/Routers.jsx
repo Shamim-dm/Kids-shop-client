27bdbf3dab7518870dbccffd9379fcac7d203aef
Shamim-DM <mdshamimmiah000000@gmail.com>
2023-05-17 23:28:09 +0600

    login page complete

--- src/Router/Routers.jsx
+++ src/Router/Routers.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Login/Register";
 
 const router = createBrowserRouter([
   {
@@ -13,7 +15,16 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>
+        },
+        {
+            path: 'login',
+            element: <Login></Login>
+        },
+        {
+            path: 'register',
+            element: <Register></Register>
         }
+
 
     ]
   },
