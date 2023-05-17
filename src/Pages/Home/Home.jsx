0ab19a66dfafe1571845d9bfb0f7615b6db72501
Shamim-DM <mdshamimmiah000000@gmail.com>
2023-05-18 01:27:53 +0600

    footer section complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Sheared/Navbar/Navbar';
 import Banner from './Banner/Banner';
+import Footer from '../Sheared/Footer/Footer';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
